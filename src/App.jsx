17dbdc799c83ff87b743c0fb459854844089e654
Brandon Chau <brandon.chau578@gmail.com>
2024-11-04 01:20:17 -0800

    feat:added the layout of the content

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
-import { useState } from 'react'
-import './App.css'
-import Sidebar from './assets/components/Sidebar'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Layout from './assets/components/Layout'; // Import the new Layout component
+import Homepage from './assets/components/Homepage';
+import CreateGuildMember from './assets/components/CreateGuildMember';
+import GuildMemberList from './assets/components/GuildMemberList';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className = "pageContainer">
-      <Sidebar/>
-    </div>
-  )
-
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/create-guild-member" element={<CreateGuildMember />} />
+          <Route path="/guild-member-list" element={<GuildMemberList />} />
+        </Routes>
+      </Layout>
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- src/assets/components/Sidebar.jsx
+++ src/assets/components/Sidebar.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import './Sidebar.css'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Sidebar.css';
+
 function Sidebar() {
   return (
-    <div className = "sideBarContainer">
-        <nav className = "sideBarOptions">
-            <a href ="">Home</a>
-            <a href ="">Create a Guild Member</a>
-            <a href ="">Guild Member List</a>
-
-        </nav>
+    <div className="sideBarContainer">
+      <h2>Realm Roster</h2>
+      <nav className="sideBarOptions">
+        <Link to="/">Home</Link>
+        <Link to="/create-guild-member">Create a Guild Member</Link>
+        <Link to="/guild-member-list">Guild Member List</Link>
+      </nav>
     </div>
-  )
+  );
 }
 
-export default Sidebar
+export default Sidebar;
